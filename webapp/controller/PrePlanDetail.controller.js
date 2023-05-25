872bd78d18ee66245f792ac07846de8fa557279e
Giriprasad Yedulla <giriprasad.yedulla@evorait.com>
2023-05-25 10:54:55 +0000

    removed unwanted variable

--- webapp/controller/PrePlanDetail.controller.js
+++ webapp/controller/PrePlanDetail.controller.js
@@ -1247,7 +1247,7 @@ sap.ui.define([
 		 * Refresh detail header forcefully
 		 */
 		_refrshDetailHeader: function () {
-			this.getOwnerComponent().readData(this._oContext.getPath()).then(function (oData){
+			this.getOwnerComponent().readData(this._oContext.getPath()).then(function (){
 				//Refreshing Status Dropdown
 				this._rebindPage();
 			}.bind(this));

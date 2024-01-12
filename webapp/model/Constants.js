e914354a233fef9eb221c49c198bdb5f9a22b618
Chethan RK <chethan.rk@evorait.com>
2024-01-12 08:50:36 +0000

    EVOSUITE2-5426 Ticket

--- webapp/model/Constants.js
+++ webapp/model/Constants.js
@@ -83,8 +83,8 @@ sap.ui.define([], function () {
 		},
 		TIMEFORMATS: {
 			0:	"HH:mm:ss",
-			1:	"HH:mm:ss a",
-			2:	"HH:mm:ss a",
+			1:	"KK:mm:ss a",
+			2:	"KK:mm:ss a",
 			3:	"KK:mm:ss a",
 			4:	"KK:mm:ss a"
 		}
